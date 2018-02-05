661df37f303497b3be5c16ee7e49a092259203a8
Milan van As <milan@hetwonen.nl>
2018-02-05 15:29:43 +0100

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpackMerge = require('webpack-merge');
 
 // Config
 module.exports = (env) => {
-    if(typeof env !== 'object' || !'env' in env){
+    if (typeof env !== 'object' || !'env' in env) {
         throw new TypeError('Provide --env.env=prod or dev in your command');
     }
     const envConfig = require(`./build-utils/webpack.${env.env}`)(env);
