e7730860ba003120c1ff35fc08d55bce995a8e51
Milan van As <milan@hetwonen.nl>
2018-02-09 15:20:23 +0100

    Added mocha to eslint

--- app/modules/Core/Core.test.js
+++ app/modules/Core/Core.test.js
@@ -1,10 +1,11 @@
-const assert = require('assert');
+import * as assert from 'assert';
 
-import { AppModule } from '../../lib/AppModule'
-import { Core } from './Core';
+import AppModule from '../../lib/AppModule';
+import Core from './Core';
+
 
 describe('Core', () => {
     it('must an instance of AppModule', () => {
-        assert.equal(new Core instanceof AppModule, true);
+        assert.equal(new Core() instanceof AppModule, true);
     });
 });
