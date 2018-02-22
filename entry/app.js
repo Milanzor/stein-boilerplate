a4bc30ef23c0896d9123ea237bec721a51637a16
Milan van As <milan@hetwonen.nl>
2018-02-22 12:33:38 +0100

    Added todos

--- entry/app.js
+++ entry/app.js
@@ -2,4 +2,8 @@ import { Dispatch } from 'stein';
 
 import Core from '../app/modules/Core/Core';
 
+// TODO A CONTROLLER IS AN ENTRY POINT!!!!!!
+// TODO REGISTER THE CONTROLLER TO A ROUTE
+// TODO DISPATCH REGISTRY
+
 Dispatch([Core]);
