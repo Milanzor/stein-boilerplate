b23da17189a4f20956749a1580f4b7600113a1f8
Milan van As <milan@hetwonen.nl>
2018-03-16 16:20:56 +0100

    Woops, typo in throwing an Error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpackMerge = require('webpack-merge');
 // Config
 module.exports = (env) => {
     if (typeof env !== 'object' || !'env' in env) {
-        throw new Errorw('Provide --env.env=prod or dev in your command');
+        throw new Error('Provide --env.env=prod or dev in your command');
     }
     const envConfig = require(`./build-utils/webpack.${env.env}`)(env);
     const commonConfig = require(`./build-utils/webpack.common`)(env);
